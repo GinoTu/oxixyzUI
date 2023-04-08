60df731cfbb131cf27479493b28304cb9052c123
GinoTu <gino.tu130@gmail.com>
2023-04-08 19:41:41 +0800

    Gino BT 2

---
+++ app/src/main/java/com/dope/ooxixyz/BLEDevice.kt
@@ -0,0 +1,4 @@
+package com.dope.ooxixyz
+
+class BLEDevice {
+}
---
+++ app/src/main/java/com/dope/ooxixyz/btlist.kt
@@ -0,0 +1,11 @@
+package com.dope.ooxixyz
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class btlist : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_btlist)
+    }
+}
